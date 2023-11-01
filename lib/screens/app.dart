462be22c84dee5145bab8f997eb4319cd185c9c2
Patryk <pludowskipatryk@gmail.com>
2023-11-01 21:31:10 +0100

    Init repository

---
+++ lib/screens/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TaskPad',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}

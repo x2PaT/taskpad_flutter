584dc0e34fa9baa3159234f8a8b09193d33fcdc4
Patryk <pludowskipatryk@gmail.com>
2023-11-05 10:24:18 +0100

    Fix types in ITasksRepository

--- lib/repository/tasks_repository_interface.dart
+++ lib/repository/tasks_repository_interface.dart
@@ -2,8 +2,9 @@ import 'package:taskpad_flutter/models/task_model.dart';
 import 'package:taskpad_flutter/models/tasks_list_model.dart';
 
 abstract class ITasksRepository {
-  void addTasksList(TasksListModel tasksListModel);
-  void addTask(int listID, TaskModel taskModel);
+ Future<void> initBoxes();
+  Future<void> addTasksList(TasksListModel tasksListModel);
+  Future<void> addTask(int listID, TaskModel taskModel);
   List<TasksListModel> getTasksListModels();
   List<TaskModel> getTasksForListID(int listID);
 }
